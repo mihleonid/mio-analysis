3a0d953bf5279b189262cd5c8c7f74f04ab6682b
Mihleonid <mihleonid@gmail.com>
2022-03-03 20:28:09 +0300

    Add arduino files

---
+++ arduino/simple-reader.cc
@@ -0,0 +1,14 @@
+/*
+ * This code can be used for simple reading signal from A0 and writing to serial port
+ */
+void setup() {
+	Serial.begin(9600);
+}
+void loop() {
+	short val=analogRead(A0);
+	char a=((val&0b11111)<<1)|1;
+	char b=(val&0b1111100000)>>4;
+	Serial.print(a);
+	Serial.print(b);
+	delay(250);
+}
