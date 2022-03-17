88a37f8618fb012fa4b3302b1a7e9e49851302b7
Mihleonid <mihleonid@gmail.com>
2022-03-17 15:25:32 +0300

    Turned into esp32

--- arduino/simple-reader.ino
+++ arduino/simple-reader.ino
@@ -1,14 +1,19 @@
 /*
- * This code can be used for simple reading signal from A0 and writing to serial port
+ * This code can be used for simple reading signal from 34 and writing to serial port
  */
 void setup() {
-	Serial.begin(9600);
+	Serial.begin(115200);
+	pinMode(34, INPUT);
 }
+int val;
 void loop() {
-	short val=analogRead(A0);
+	val=analogRead(34);
+	Sreial.prinln(val);
+	/*
 	char a=((val&0b11111)<<1)|1;
 	char b=(val&0b1111100000)>>4;
 	Serial.print(a);
 	Serial.print(b);
+	*/
 	delay(20);
 }
---
+++ arduino/wifi-server-reader.ino
@@ -0,0 +1,70 @@
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
+
+const char* ssid = "TP-Link_37AB";
+const char* password = "PASS";
+
+WebServer server(80);
+
+void handleRoot() {
+  server.send(200, "text/plain", "Use /13 to monitor 13 pin");
+}
+
+void handleNotFound() {
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+}
+
+void setup(void) {
+  pinMode(13, INPUT);
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (MDNS.begin("esp32")) {
+    Serial.println("MDNS responder started");
+  }
+
+  server.on("/", handleRoot);
+
+  server.on("/13", []() {
+    int val;
+    val=analogRead(34);
+    server.send(200, "text/plain", String(val));
+  });
+
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop(void) {
+  server.handleClient();
+  Serial.println(analogRead(34));
+  delay(2);//allow the cpu to switch to other tasks
+}
