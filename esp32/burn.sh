a4047ac2d9fd116e1ddd8ac1ef3968edf6bd4e13
Mihleonid <mihleonid@gmail.com>
2022-05-25 17:43:00 +0300

    Add shell script for composition of all modules

---
+++ esp32/burn.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo "//IT IS A BURN FILE. DO NOT EDIT." > ../burn.ino
+cat adc.ino >> ../burn.ino
+cat i2clcd.ino >> ../burn.ino
+cat display.ino >> ../burn.ino
+cat main.ino >> ../burn.ino
