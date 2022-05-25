f0e58df0a56a780ebefa7bf4584534c2f019cbcb
Mihleonid <mihleonid@gmail.com>
2022-05-25 17:41:16 +0300

    Add ADC header

---
+++ esp32/adc.ino
@@ -0,0 +1,13 @@
+#include <Adafruit_ADS1015.h>
+Adafruit_ADS1115 ads(0x48);
+void adc_init(){
+	ads.begin();
+}
+double adc_voltage(int channel){
+	return (adc_read(channel)*((double)0.1875))/1000;
+}
+int16_t adc_read(int channel){
+	return ads.readADC_SingleEnded(channel);
+}
+void adc_loop(){
+}
