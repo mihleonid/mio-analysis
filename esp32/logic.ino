7d9efac5fb37b40621c9dd226b6ce90a45179aeb
Mihleonid <mihleonid@gmail.com>
2022-05-26 17:32:28 +0300

    Update logic

--- esp32/logic.ino
+++ esp32/logic.ino
@@ -1,13 +1,47 @@
 #define State short
-#define MENU 0
-#define MONITOR 1
-#define METHOD 2
+#define MENU_MONITOR 0
+#define MENU_METHOD 1
+#define MONITOR 2
+#define METHOD_WIFI 3
+#define METHOD_USB 4
+#define METHOD_BLE 5
 
-State logic_current;
+
+State logic_curr;
+State logic_curr_method;
 
 void logic_init(){
-	logic_curr=MENU;
+	logic_curr=MENU_MONITOR;
 }
 void logic_loop(){
-	//TODO change states
+	Event e;
+	while((e=events_get())!=NONE){
+		if(e==WAIT){
+			e=CLICK;
+		}
+		if(logic_curr==MENU_MONITOR){
+			if(e==CLICK){
+				logic_curr=MONITOR;
+				continue;
+			}
+			if(e==ROT_POS){
+				logic_curr=MENU_METHOD;
+			}
+			if(e==ROT_NEG){
+				logic_curr=MENU_METHOD;
+			}
+		}
+		if(logic_curr==MENU_METHOD){
+			if(e==CLICK){
+				logic_curr=logic_curr_method;
+				continue;
+			}
+			if(e==ROT_POS){
+				logic_curr=MENU_MONITOR;
+			}
+			if(e==ROT_NEG){
+				logic_curr=MENU_MONITOR;
+			}
+		}
+	}
 }
