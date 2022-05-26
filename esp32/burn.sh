45812020520bf4e0d37338d1679f7f45899231c4
Mihleonid <mihleonid@gmail.com>
2022-05-26 18:01:45 +0300

    Update burn.sh

--- esp32/burn.sh
+++ esp32/burn.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-echo "//IT IS A BURN FILE. DO NOT EDIT." > ../burn.ino
-cat clock.ino >> ../burn.ino
-cat events.ino >> ../burn.ino
-cat adc.ino >> ../burn.ino
-cat i2clcd.ino >> ../burn.ino
-cat display.ino >> ../burn.ino
-cat encoder.ino >> ../burn.ino
-cat logic.ino >> ../burn.ino
-cat main.ino >> ../burn.ino
+mkdir -p ../burn
+echo "//IT IS A BURN FILE. DO NOT EDIT." > ../burn/burn.ino
+cat i2clcd.ino >> ../burn/burn.ino
+cat clock.ino >> ../burn/burn.ino
+cat events.ino >> ../burn/burn.ino
+cat adc.ino >> ../burn/burn.ino
+cat display.ino >> ../burn/burn.ino
+cat encoder.ino >> ../burn/burn.ino
+cat logic.ino >> ../burn/burn.ino
+cat main.ino >> ../burn/burn.ino
